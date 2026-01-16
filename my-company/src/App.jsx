0219d06ecc01f1fdc8f8f321f684b1957c974068
Uhiara-Chidiadi <uhiarachidiadi@gmail.com>
2026-01-16 15:07:25 +0100

    Move Contact component to src/components for checker compatibility

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Contact from './components/Contact';
 import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
-import Contact from './pages/Contact';
 import './App.css';
 
 function App() {
